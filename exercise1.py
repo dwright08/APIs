2ab55e751b16d5a8ab59fb10b34a5f93130bfaf6
Drevon Wright <dmakwright@gmail.com>
2022-01-06 22:26:39 -0500

    completed homework

--- exercise1.py
+++ exercise1.py
@@ -2,11 +2,15 @@ import geocoder
 
 def main():
   # Declare destinations list here
-  destinations = ???
+  destinations = ['Space Needle', 'Crater Lake', 'Golden Gate Bridge', 'Yosemite National Park',
+  'Las Vegas, Nevada', 'Grand Canyon National Park', 'Aspen, Colorado', 'Mount Rushmore', 'Yellowstone National Park', 
+  'Sandpoint, Idaho', 'Banff National Park', 'Capilano Suspension Bridge']
 
   # Loop through each destination
   for destination in destinations:
     #   Get the lat-long coordinates from `geocoder.arcgis`
+    location = geocoder.arcgis(destination)
     #   Print out the place name and the coordinates
-
+    print(f'{destination} is located at ({location.latlng[0]:.4f}, {location.latlng[1]:.4f})')
+    
 main()
--- exercise2.py
+++ exercise2.py
@@ -3,7 +3,7 @@ import requests
 
 # Replace XXXXXXXXXXXX with your actual key, which
 # will look like a bunch of letters and numbers!
-API_KEY = 'XXXXXXXXXXXX'
+API_KEY = XXXXXXXXXXXXXXXXXX
 # When you are done with the homework, remove your actual key
 # and replace it back with XXXXXXXXXXXXXXXXXX before you commit
 # and push to Github.
@@ -11,20 +11,29 @@ API_KEY = 'XXXXXXXXXXXX'
 API_BASE_URL = f'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid={API_KEY}'
 
 def main():
-  destinations = ???
+  destinations = ['Space Needle', 'Crater Lake', 'Golden Gate Bridge', 'Yosemite National Park',
+  'Las Vegas, Nevada', 'Grand Canyon National Park', 'Aspen, Colorado', 'Mount Rushmore', 'Yellowstone National Park', 
+  'Sandpoint, Idaho', 'Banff National Park', 'Capilano Suspension Bridge']
 
   # Loop through each destination
   for destination in destinations:
     ###########
     # COPY & PASTE RELEVANT CODE FROM PART 1 HERE
     ###########
-
+    location = geocoder.arcgis(destination)
+    print(f'{destination} is located at ({location.latlng[0]:.4f}, {location.latlng[1]:.4f})')
     # You'll need to construct the full API url with the latitude and longitude,
     # with something like this:
     # full_api_url = API_BASE_URL + '&lat=' + latitude + '&lon=' + longitude
+    
+    #latitude = f'{location.latlng[0]:.4f}'
+    #longitude = f'{location.latlng[1]:.4f}'
 
+    
+    full_api_url = f'{API_BASE_URL}&lat={location.latlng[0]:.4f}&lon={location.latlng[1]:.4f}'
+    
     result = requests.get(full_api_url).json()
-
+  
     # From the result, print out the summary and current temperature
-
+    print(f"At {destination} right now, it's {result['weather'][0]['description']} with a temperature of {result['main']['temp']}")
 main()
